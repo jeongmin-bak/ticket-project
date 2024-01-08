5539a505e4d1dd20bf13f852fa81d2aa00f803b4
jungmin.park <pjm9673@gmail.com>
2024-01-08 13:50:29 +0900

    init commit

---
+++ src/main/java/com/example/ticketproject/TicketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketproject/TicketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
